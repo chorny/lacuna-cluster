861de9fc04d9510402d10df9f8e7d8ddc4fe88c4
Alexandr Ciornii <alexchorny@gmail.com>
2010-10-17 17:45:00 +0300

    small refactoring

--- lacuna-cluster.pl
+++ lacuna-cluster.pl
@@ -4,8 +4,12 @@ use 5.010;
 use strict;
 use warnings;
 
+my $max_radius_of_cluster=100;
+my $diam=2*$max_radius_of_cluster;
+say "max radius: $max_radius_of_cluster, diameter: $diam";
+
 use Algorithm::ClusterPoints;
-my $clp = Algorithm::ClusterPoints->new(radius => 100, minimum_size => 1, ordered => 1, dimension => 2);
+my $clp = Algorithm::ClusterPoints->new(radius => $max_radius_of_cluster, minimum_size => 1, ordered => 1, dimension => 2);
 
 open my $fh,'<','colonylist.txt';
 my $current_empire='';
@@ -16,6 +20,7 @@ while (my $line=<$fh>) {
   if ($line=~/^\s*\d+\.\s*(\w.*[\w\)])$/s) {
     $current_empire=$1;
     $current_empire=~s/\(.*?\)//;
+    $current_empire=~s/\s+$//s;
     #say "$current_empire";
     $first_present=0;
   } elsif ($line=~/(-?\d+)x? , \s* (-?\d+)y?/sx) {
@@ -23,7 +28,7 @@ while (my $line=<$fh>) {
     $first_present=1;
     my ($x,$y)=($1,$2);
     #say "$x,$y - $line";
-    say "$x,$y - $current_empire";
+    #say "$x,$y - $current_empire";
     $coord2empire{"$x, $y"}=$current_empire;
     $clp->add_point($x,$y);
   }
